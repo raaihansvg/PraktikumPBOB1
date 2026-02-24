09dacdef589ca0647118982bd2abd218b0b7ac01
raaihansvg <rlazuardi163@gmail.com>
2026-02-24 20:22:37 +0700

    Pertemuan 1 done GnG

---
+++ Pertemuan1/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+   
+    public static void main(String[] args) throws Exception {
+      
+        titik T1 = new titik();   
+        T1.setAbsis(3);          
+        T1.setOrdinat(4);      
+        T1.printTitik();           
+        T1.geser(3,4);        
+        T1.printTitik();           
+    }
+}
