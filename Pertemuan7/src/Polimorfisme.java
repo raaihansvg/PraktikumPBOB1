d5dd5a86bd5dabe2dc1c76e87fc48f33b7304b09
raaihansvg <rlazuardi163@gmail.com>
2026-04-13 11:20:54 +0700

    4

--- Pertemuan7/src/Polimorfisme.java
+++ Pertemuan7/src/Polimorfisme.java
@@ -16,19 +16,42 @@ public class Polimorfisme{
         return a/b;
     }
 
+    public static double tambahKoma(double x, double y){
+        return x + y;
+    }
+
 
     public static void main(String[] args){
+        /* ------------------------------------------------------------------------- */
+        /* ---------------------------FUNCTION 1------------------------------------ */
+        /* ------------------------------------------------------------------------- */
         Integer bilangan = 10;
 
         int hasilKuadrat = kuadrat(bilangan);
 
         System.out.printf("Hasil kuadrat %d adalah %d\n", bilangan, hasilKuadrat);
-
+        /* ------------------------------------------------------------------------ */
+        /* ---------------------------FUNCTION 2---------------------------------- */
+        /* ------------------------------------------------------------------------ */
         int x = 10;
         int y = 5;
 
         double hasil = bagi(x,y);
 
         System.out.printf("Hasil bagi %d dengan %d adalah %f\n", x,y,hasil);
+        /* -------------------------------------------------------------------------- */
+        /* ---------------------------FUNCTION 3------------------------------------ */
+        /* -------------------------------------------------------------------------- */
+        int b = 4;
+        int c = 7;
+
+        double hasilTambahKoma = tambahKoma(b, c);
+        System.out.printf("Hasil tambah koma dari %d dan %d adalah %f\n", b,c,hasilTambahKoma);
+          /* -------------------------------------------------------------------------- */
+        /* ---------------------------FUNCTION 4------------------------------------ */
+        /* -------------------------------------------------------------------------- */
+        char huruf = 'A';
+        int angka = huruf;
+        System.out.println("ASCII: " + angka);
     }
 }
